e34f8674cb81f9eecdcd3ba4359ee3fa46cbb879
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-01-13 12:03:23 -0500

    feat: change `statuscafe-author` element

--- src/scripts/current-status.js
+++ src/scripts/current-status.js
@@ -2,7 +2,8 @@ fetch('https://status.cafe/users/kirjahri/status.json')
   .then((response) => response.json())
   .then((data) => {
     document.getElementById('statuscafe-face').innerHTML = data.face;
-    document.getElementById('statuscafe-author').innerHTML = data.author;
+    document.getElementById('statuscafe-author').innerHTML =
+      `<a href="https://status.cafe/users/kirjahri"><b>${data.author}</b></a>`;
     document.getElementById('statuscafe-time-ago').innerHTML = data.timeAgo;
     document.getElementById('statuscafe-content').innerHTML = data.content;
   });
