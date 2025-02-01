df5569f829a9967433785c7f37480d7480f009f4
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-02-01 12:33:49 -0500

    feat: add animation for currently active sidebar link

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,34 +1,16 @@
+<script lang="ts">
+  import SidebarLink from './SidebarLink.svelte';
+</script>
+
 <nav>
   <h1>sidebar</h1>
   <ul>
-    <li><a href="/">home</a></li>
+    <SidebarLink path="/" name="home" />
   </ul>
 </nav>
 
 <style>
   ul {
     list-style-type: none;
-  }
-
-  li {
-    text-align: center;
-    margin: 5px 0;
-  }
-
-  li:last-child {
-    margin-bottom: 0;
-  }
-
-  li a {
-    display: block;
-    padding: 5px;
-    color: var(--primary-color);
-    background-color: var(--secondary-color);
-    border-radius: 5px;
-    text-decoration: none;
-  }
-
-  li a:hover {
-    text-decoration: underline;
   }
 </style>
---
+++ src/lib/components/SidebarLink.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { page } from '$app/state';
+
+  let { path, name } = $props();
+
+  let active = $derived(page.url.pathname === path);
+</script>
+
+<li><a href={path} class:active>{name}</a></li>
+
+<style>
+  li {
+    text-align: center;
+    margin: 5px 0;
+  }
+
+  li:last-child {
+    margin-bottom: 0;
+  }
+
+  li a {
+    display: block;
+    padding: 5px;
+    color: var(--secondary-color);
+    background-color: var(--primary-color);
+    border: 1px solid var(--secondary-color);
+    border-radius: 5px;
+    text-decoration: none;
+  }
+
+  li a:hover {
+    text-decoration: underline;
+  }
+
+  li a.active {
+    color: var(--primary-color);
+    background-color: var(--secondary-color);
+    border: 1px solid var(--secondary-color);
+    animation: bounce-in-out 0.3s;
+    cursor: default;
+  }
+
+  li a:not(.active):active {
+    transform: scale(0.9);
+    transition: 0.15s;
+  }
+
+  @keyframes bounce-in-out {
+    from {
+      transform: scale(0.9);
+    }
+
+    50% {
+      transform: scale(1.015);
+    }
+
+    to {
+      transform: scale(1);
+    }
+  }
+</style>
