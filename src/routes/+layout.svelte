56f9968f20ae4b4edd03dc301da90249b9276da8
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-02-02 15:09:04 -0500

    feat: add box shadow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -104,6 +104,7 @@
     border: var(--border-width) solid var(--secondary);
     border-radius: var(--border-radius);
     background-color: var(--primary);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   }
 
   .container > :global(*) {
