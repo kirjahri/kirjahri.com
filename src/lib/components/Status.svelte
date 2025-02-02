5ac2c6a0b08e8b61e1c8ca60da82d9233da4309f
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-02-02 12:21:37 -0500

    feat: create status component

---
+++ src/lib/components/Status.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let data: any = $state({});
+
+  fetch('https://status.cafe/users/kirjahri/status.json')
+    .then((response) => response.json())
+    .then((json) => (data = json));
+</script>
+
+<section>
+  <div class="status">
+    <div class="status-username">
+      <a href="https://status.cafe/users/kirjahri" target="_blank"
+        >{data.author}</a
+      >
+      {data.face}
+      {data.timeAgo}
+    </div>
+    <div class="status-content">{data.content}</div>
+  </div>
+</section>
+
+<style>
+</style>
