fc523064efcf5de797384e84b2d0ba0eceff137e
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-02-09 12:15:47 -0500

    feat(components): add header to status

--- src/lib/components/Status.svelte
+++ src/lib/components/Status.svelte
@@ -10,21 +10,28 @@
 </script>
 
 <section>
-  <div class="status">
-    <div class="status-username">
-      <ExternalLink
-        href={`https://status.cafe/users/${username}`}
-        content={data.author || username}
-      /> &bull;
-      {data.face || '...'} &bull;
-      {data.timeAgo || '...'}
+  <h1>
+    <ExternalLink href="https://status.cafe" content="status.cafe" />
+  </h1>
+  <hr />
+  <section>
+    <div class="status">
+      <div class="status-username">
+        <ExternalLink
+          href={`https://status.cafe/users/${username}`}
+          content={data.author || username}
+        /> &bull;
+        {data.face || '...'} &bull;
+        {data.timeAgo || '...'}
+      </div>
+      <div class="status-content">{@html data.content || '...'}</div>
     </div>
-    <div class="status-content">{@html data.content || '...'}</div>
-  </div>
+  </section>
 </section>
 
 <style lang="scss">
-  section {
+  section section {
+    border-style: solid;
     padding: 0;
   }
 
@@ -32,6 +39,7 @@
     padding: $padding;
     color: $primary;
     background-color: $secondary;
+    border-radius: 5px 5px 0 0;
     margin-bottom: 5px;
   }
 
