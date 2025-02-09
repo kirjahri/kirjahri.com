ecb3f2e8b3bc5bf3002911d48bb1844e0cc3b7d1
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-02-09 11:21:18 -0500

    feat(components): create `ExternalLink.svelte`

---
+++ src/lib/components/ExternalLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { IconExternalLink } from '@tabler/icons-svelte';
+
+  export let href: string;
+  export let target: string = '_blank';
+  export let content: string;
+</script>
+
+<a {href} {target}>{content} <IconExternalLink /></a>
+
+<style lang="scss">
+  a :global(svg) {
+    width: 16px;
+    height: 16px;
+    stroke-width: 3;
+  }
+</style>
